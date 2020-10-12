4dafd53ac95dc2018ca3934b546dbd0a362f1f15
Alejandro Quesada <alejosequesada@gmail.com>
2020-10-12 15:09:26 -0600

    Add test methods for linked list
    Implements a test fixture for Doubly-Linked Lists, by testing each method of said structure invididually.

--- ServerTest/example.cpp
+++ ServerTest/example.cpp
@@ -8,13 +8,59 @@ int main(int argc, char *argv[])
     ::testing::InitGoogleTest(&argc, argv);
         return RUN_ALL_TESTS();
 }
-
-
-TEST(ListasDobles , Insercion )
-    {
-              DoubleList<int>* miListaHiperEspecial= new DoubleList<int>;
-              int prueba = 12;
-              miListaHiperEspecial->add(prueba);
-
-              EXPECT_EQ(*miListaHiperEspecial->get(0),12);
+struct ListTest : testing::Test {
+    DoubleList<int> * testList;
+    ListTest() {
+        testList = new DoubleList<int>;
     }
+
+};
+TEST_F(ListTest, ListStartsEmpty) {
+    EXPECT_EQ(0, testList->getLen());
+}
+
+TEST_F(ListTest, ListHasNohead) {
+    // as in this test no element has been added, the list should have nothing as index 0.
+    EXPECT_ANY_THROW(testList->get(0));
+}
+
+TEST_F(ListTest, ListReturnsCorrectValue) {
+    int a = 2;
+    testList->add(a);
+    EXPECT_EQ(2, * testList->get(0));
+}
+
+TEST_F(ListTest, ListInsertion) {
+    int b = 5;
+    testList->add(b);
+    EXPECT_EQ(5, *testList->get(0));
+}
+
+TEST_F(ListTest, ListSecondInsertion) {
+    int a = 2;
+    int b = 10;
+    testList->add(a);
+    testList->add(b);
+    EXPECT_EQ(10, * testList->get(1));
+}
+
+TEST_F(ListTest, ListDeleteElement) {
+    int a = 2;
+    int b = 10;
+    testList->add(a);
+    testList->add(b);
+    int c = 15;
+    testList->add(c);
+    // at this point we should have 3 elements in the list structure
+    int tempLength = testList->getLen();
+    testList->erase(tempLength - 1); //this should remove the last element from the list
+    EXPECT_EQ(tempLength - 1, testList->getLen());
+}
+
+TEST_F(ListTest, ElementValueChanging) {
+    int a = 15;
+    testList->add(a);
+    int c = 10;
+    testList->set(0, c);
+    EXPECT_EQ(10, * testList->get(0));
+}

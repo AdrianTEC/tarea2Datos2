bfa3405c2d333d4e064557cfb17009b223c451f8
adrian <adriantec2019@gmail.com>
2020-10-11 19:11:05 -0600

    intentando importar gtest

---
+++ build-TestClient-Desktop_Qt_5_15_1_GCC_64bit-Debug/googletest-build/googlemock/CMakeFiles/gmock_main.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/adrian/Escritorio/tarea2Datos2/build-TestClient-Desktop_Qt_5_15_1_GCC_64bit-Debug/googletest-build/googlemock/gmock_main_autogen/mocs_compilation.cpp" "/home/adrian/Escritorio/tarea2Datos2/build-TestClient-Desktop_Qt_5_15_1_GCC_64bit-Debug/googletest-build/googlemock/CMakeFiles/gmock_main.dir/gmock_main_autogen/mocs_compilation.cpp.o"
+  "/home/adrian/Escritorio/tarea2Datos2/build-TestClient-Desktop_Qt_5_15_1_GCC_64bit-Debug/googletest-src/googlemock/src/gmock_main.cc" "/home/adrian/Escritorio/tarea2Datos2/build-TestClient-Desktop_Qt_5_15_1_GCC_64bit-Debug/googletest-build/googlemock/CMakeFiles/gmock_main.dir/src/gmock_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "googletest-build/googlemock"
+  "googletest-build/googlemock/gmock_main_autogen/include"
+  "googletest-src/googlemock"
+  "googletest-src/googlemock/include"
+  "googletest-src/googletest/include"
+  "googletest-src/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/adrian/Escritorio/tarea2Datos2/build-TestClient-Desktop_Qt_5_15_1_GCC_64bit-Debug/googletest-build/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake"
+  "/home/adrian/Escritorio/tarea2Datos2/build-TestClient-Desktop_Qt_5_15_1_GCC_64bit-Debug/googletest-build/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
